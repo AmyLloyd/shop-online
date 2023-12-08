72756d5f271ace671f4fa159dfdc89938e3b410c
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-12-08 23:03:44 +1030

    debug connection and category routes

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,10 @@ console.log(process.env.DB_NAME);
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("ecommerce_db", "root", "_pie083OUT()", {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
